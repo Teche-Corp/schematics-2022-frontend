614fe78b05fe82c5789504ea095f527ce57a029f
bazoka-kaka <yehezkielwiradhika@gmail.com>
2022-07-12 00:26:51 +0700

    feat: add nav responsive

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,17 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { FaAngleDown } from "react-icons/fa";
+import { FaAngleDown, FaTimes } from "react-icons/fa";
 import { MdOutlineMonitor } from "react-icons/md";
 import { BiBrain } from "react-icons/bi";
 import { HiSpeakerphone } from "react-icons/hi";
 import { BsStars } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Nav = () => {
+  const [showNav, setShowNav] = useState(false);
   const [showEvents, setShowEvents] = useState(false);
   const [hideEvents, setHideEvents] = useState(true);
   const toggleShowEvents = () => {
     setShowEvents((prev) => !prev);
+  };
+  const toggleShowNav = () => {
+    setShowNav((prev) => !prev);
   };
   useEffect(() => {
     if (showEvents) {
@@ -24,11 +29,11 @@ const Nav = () => {
   }, [showEvents]);
   return (
     <div className="fixed z-50">
-      <div className="bg-white absolute w-screen h-[92px] opacity-[0.15]"></div>
+      <div className="bg-white absolute w-screen h-[92px] z-20 opacity-[0.15]"></div>
       <nav className="h-[92px] relative z-50 select-none backdrop-blur-3xl w-screen font-secondary flex justify-between items-center px-5 md:px-16 lg:px-[132px]">
         {/* header logo */}
         <Link href="/">
-          <a className="flex gap-1">
+          <a className="relative z-50 flex gap-1">
             <Image alt="" src="/schematics-logo.png" width={72} height={72} />
             <div className="flex flex-col justify-center">
               <h1 className="font-bold leading-6 text-white">
@@ -39,6 +44,20 @@ const Nav = () => {
             </div>
           </a>
         </Link>
+        <div className="relative z-50 flex mr-2 md:hidden">
+          {!showNav && (
+            <GiHamburgerMenu
+              className="text-4xl text-white"
+              onClick={toggleShowNav}
+            />
+          )}
+          {showNav && (
+            <FaTimes
+              className="text-4xl text-red-600"
+              onClick={toggleShowNav}
+            />
+          )}
+        </div>
         <ul className="md:flex hidden text-white md:gap-5 lg:gap-10 xl:gap-[67px]">
           <li className="relative">
             <button
@@ -143,6 +162,76 @@ const Nav = () => {
             </Link>
           </li>
         </ul>
+
+        {/* {showNav && ( */}
+        <ul
+          className={`flex md:hidden translate-y-full ${
+            showNav ? "translate-x-0 opacity-1" : "translate-x-full opacity-0"
+          } text-white z-20 transition duration-300 md:flex-row flex-col bg-[#464646] md:static absolute bottom-0 left-0 md:w-auto w-full md:gap-5 lg:gap-10 xl:gap-[67px]`}
+        >
+          <li className="relative py-4 border-2 border-black px-7">
+            <button
+              onClick={toggleShowEvents}
+              className="flex items-center justify-between w-full md:w-auto md:gap-[6px] text-3xl font-primary text-border-thin md:font-secondary md:text-md"
+            >
+              Events{" "}
+              <FaAngleDown
+                className={`mt-1 ${
+                  showEvents ? "rotate-180" : "rotate-0"
+                } transition duration-150 text-3xl md:text-lg`}
+              />
+            </button>
+          </li>
+          <div className={`${showEvents ? "flex" : "hidden"} flex-col w-full`}>
+            <div className="flex h-10 gap-5 bg-white border-2 border-black px-7">
+              <Link href="/">
+                <a className="inline-block w-full text-2xl text-black font-primary">
+                  <div className="inline-block w-5 h-5 rounded-full bg-nlc"></div>{" "}
+                  Schematics <span className="text-nlc">NLC</span>
+                </a>
+              </Link>
+            </div>
+            <div className="flex h-10 gap-5 bg-white border-2 border-black px-7">
+              <Link href="/">
+                <a className="inline-block w-full text-2xl text-black font-primary">
+                  <div className="inline-block w-5 h-5 rounded-full bg-npc"></div>{" "}
+                  Schematics <span className="text-npc">NPC</span>
+                </a>
+              </Link>
+            </div>
+            <div className="flex h-10 gap-5 bg-white border-2 border-black px-7">
+              <Link href="/">
+                <a className="inline-block w-full text-2xl text-black font-primary">
+                  <div className="inline-block w-5 h-5 rounded-full bg-nst"></div>{" "}
+                  Schematics <span className="text-nst">NST</span>
+                </a>
+              </Link>
+            </div>
+            <div className="flex h-10 gap-5 bg-white border-2 border-black px-7">
+              <Link href="/">
+                <a className="inline-block w-full text-2xl text-black font-primary">
+                  <div className="inline-block w-5 h-5 rounded-full bg-reeva"></div>{" "}
+                  Schematics <span className="text-reeva">REEVA</span>
+                </a>
+              </Link>
+            </div>
+          </div>
+          <li className="py-4 border-2 border-black px-7">
+            <Link href="/#about">
+              <a className="flex items-center justify-between w-full md:w-auto md:gap-[6px] text-3xl font-primary text-border-thin md:font-secondary md:text-md">
+                About Us
+              </a>
+            </Link>
+          </li>
+          <li className="py-4 bg-white border-2 border-black px-7">
+            <Link href="/">
+              <a className="flex items-center justify-center w-full md:w-auto md:gap-[6px] text-3xl font-primary text-black md:font-secondary md:text-md">
+                Login
+              </a>
+            </Link>
+          </li>
+        </ul>
+        {/* )} */}
       </nav>
     </div>
   );
