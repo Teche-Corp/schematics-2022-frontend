23ebeeaf00b1a09bfd44cba083333df4a4846202
akmalf06 <akmaldavinci06@gmail.com>
2022-07-16 23:54:19 +0700

    feat: add reeva and nst coming soon page

---
+++ pages/nst.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import Head from 'next/head';
+
+export default function NST() {
+  return (
+    <>
+        <Head>
+            <title>Schematics NST 2022</title>
+            <meta name="description" content="Schematics 2022" />
+            <link rel="icon" href="/schematics-logo.png" />
+        </Head>
+        <img 
+            src="/nst/nst-top-left.svg" 
+            alt="nst-top-left"
+            className='absolute right-0 top-0 w-3/12' 
+        />
+        <img 
+            src="/nst/nst-top-right.svg" 
+            alt="nst-top-right"
+            className='absolute left-0 top-0 w-3/12' 
+        />
+        <div className='h-screen flex flex-col justify-center items-center md:pb-16 pb-0' style={{background: 'linear-gradient(180deg, rgba(37, 37, 37, 0.8) 0%, #252525 100%)'}}>
+            <div className='md:w-1/2 w-4/5 text-center'>
+                <Image
+                    src="/nst/nst-text.svg"
+                    alt="nst text"
+                    width={614}
+                    height={220}
+                />
+            </div>
+            <div className='md:w-1/2 w-4/5 mt-2 text-white font-primary text-center space-y-2'>
+                <p className='md:text-5xl text-3xl uppercase'>
+                    is coming soon
+                </p>
+                <p className='md:text-5xl text-3xl uppercase'>
+                    01.08.22
+                </p>
+            </div>
+            <Link 
+                href="/"
+            >
+                <button className='w-[270px] h-[50px] mt-12 rounded-md bg-nst-2 text-white font-primary text-xl'>
+                    Back
+                </button>
+            </Link>
+        </div>
+        <img 
+            src="/nst/nst-bottom.svg" 
+            alt="nst-bottom"
+            className='absolute w-full bottom-0' 
+        />
+    </>
+  )
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,20 @@ module.exports = {
           DEFAULT: "#151414",
           primary: "#252525",
         },
+        reeva: {
+          1: "#3C8ACA",
+          2: "#70A5D1",
+          3: "#2E97EE",
+          4: "#78C0FC",
+          5: "#ACE6FF"
+        },
+        nst: {
+          1: "#AEE6CB",
+          2: "#85D4BE",
+          3: "#96DFD8",
+          4: "#DAF1DE",
+          5: "#C3DDE0"
+        }
       },
       fontFamily: {
         primary: "Fredoka One, cursive",
