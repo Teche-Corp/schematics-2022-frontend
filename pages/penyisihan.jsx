2d55ed7b49297238e0669d61b2cb238221c62bfb
akmalf06 <akmaldavinci06@gmail.com>
2022-09-26 11:16:21 +0700

    fix: using next/link instead of a

--- pages/penyisihan.jsx
+++ pages/penyisihan.jsx
@@ -4,6 +4,7 @@ import ScoreBoardTable from "../components/ScoreboardTable";
 import { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import Head from "next/head";
+import Link from "next/link";
 
 export default function WarmingUp() {
   const [data, setData] = useState([]);
@@ -27,9 +28,11 @@ export default function WarmingUp() {
       <Nav/>
       <main className="bg-dark-primary h-screen w-full flex items-center justify-center flex-col gap-y-4 md:gap-y-8">
         <h1 className="font-primary text-white text-6xl">Schematics NLC 2022</h1>
-        <a href='/warming-up'>
-          <button className="text-white font-secondary text-2xl hover:text-yellow-400 bg-stone-700 px-3 py-1 rounded-lg active:brightness-90">WARMING UP</button>
-        </a>
+        <Link href='#'>
+          <a>
+            <button className="text-white font-secondary text-2xl hover:text-yellow-400 bg-stone-700 px-3 py-1 rounded-lg active:brightness-90">WARMING UP</button>
+          </a>
+        </Link>
         <h2 className="text-white text-3xl font-medium font-secondary">Babak Penyisihan</h2>
         <div className="w-3/4">
           <div className="flex items-center">
